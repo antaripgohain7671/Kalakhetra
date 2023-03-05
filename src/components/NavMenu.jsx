7d08970f4fa41d63f7bfc66b4b4d35a3956c5989
antaripgohain7671 <antarip.767@gmail.com>
2023-03-05 08:31:02 +0530

    first commit

---
+++ src/components/NavMenu.jsx
@@ -0,0 +1,13 @@
+
+function NavMenu() {
+  return (
+    <nav>
+        <ul className="hidden lg:flex lg:space-x-7 lg:ml-14">
+        <li className="cursor-pointer hover:text-bk-1">About</li>
+        <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavMenu
